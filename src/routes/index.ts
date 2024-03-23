93929b7e111652e7ecd86e5888a77f85101863a6
WSenci <wsa.wes05@gmail.com>
2024-03-23 13:23:23 -0300

    fazendo validação de usuário logado por token

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,7 +1,11 @@
 import { Router } from "express";
 import taskRoutes from './task/task.routes';
+import authRoutes from './auth/auth.routes';
 
 const routes = Router();
+
+routes.use('/task', taskRoutes);
+routes.use('/auth', authRoutes);
 
 routes.use('/task', taskRoutes);
 
--- src/routes/task/task.routes.ts
+++ src/routes/task/task.routes.ts
@@ -1,10 +1,12 @@
 import { Router } from "express";
 import TaskController from '../../controllers/task/task.controller';
+import authMiddleware from "../../middlewares/auth.middleware";
 
 const taskRoutes = Router();
 
 taskRoutes.post('/', TaskController.store);
 taskRoutes.get('/', TaskController.index);
+taskRoutes.get('/', authMiddleware, TaskController.show);
 taskRoutes.get('/:id', TaskController.show);
 taskRoutes.delete('/:id', TaskController.delete);
 taskRoutes.put('/:id', TaskController.update);
